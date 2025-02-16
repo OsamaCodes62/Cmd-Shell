cd09c322ac6630e6992e1d74c3e67651b6677920
OsamaCodes62 <ocarts62@gmail.com>
2025-02-16 14:42:15 +0500

    Add PATHs

--- commad_Shell.py
+++ commad_Shell.py
@@ -1,6 +1,21 @@
 import os
 
 builtin = ["echo", "exit 0", "type"]
+extension = [".exe", ".bat", ".cmd"]
+def getPath(cmd):
+    paths = os.getenv("PATH").split(os.pathsep)
+    for path in paths:
+        if os.name == "nt":
+            for exe in extension:
+                fullpath = os.path.join(path, cmd+exe)
+                if os.path.exists(fullpath):
+                    return fullpath
+        else:
+            fullpath = os.path.join(path, cmd)
+            if os.path.exists(fullpath):
+                return fullpath
+
+    return None
 
 while True:
     user_input = input("$ ")
@@ -11,11 +26,12 @@ while True:
         case ["echo", *arg]:
             print(*arg)
         case ["type", *arg]:
-            temp = arg
-            print(type(temp))
             forType = " ".join(arg)
+            thePath = getPath(forType)
             if forType in builtin:
                 print(forType, "is shell builtin command")
+            elif thePath:
+                print(f"{forType} is {thePath}")
             else:
                 print(forType, ": command not found")
         case _:
